bccdfe36f373c4789d29b3e3b9d39854bc4b5ffa
giovanigalego <giovanigalego96@gmail.com>
2022-07-06 13:59:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("my parents new healthy dinner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Blueberry oatmeal")
+streamlit.text("kale, spinach and rocket smoothie")
+streamlit.text("hard-boiled free-range egg")
