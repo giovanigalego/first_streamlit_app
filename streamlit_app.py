ccdcb320c90fe2812860cb20b72f4cf21ab502be
giovanigalego <giovanigalego96@gmail.com>
2022-07-08 17:29:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ try:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
         fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
-except URLError e:
+except URLErrors as e:
     streamlit.error()
 
 
